0f0d04c1ae6bda35d98722b6a38eaed82c447202
shufangreal <shufangreal@126.com>
2021-05-31 08:11:40 +0800

    this is my 8 commit

--- src/main/java/com/shufang/hive/FlinkAndHiveIntegration.java
+++ src/main/java/com/shufang/hive/FlinkAndHiveIntegration.java
@@ -1,13 +1,21 @@
 package com.shufang.hive;
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.java.StreamTableEnvironment;
 import org.apache.flink.table.catalog.hive.HiveCatalog;
+import org.apache.flink.types.Row;
 
 public class FlinkAndHiveIntegration {
-    public static void main(String[] args) {
-        EnvironmentSettings settings = EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build();
-        TableEnvironment tableEnv = TableEnvironment.create(settings);
+    public static void main(String[] args) throws Exception {
+        EnvironmentSettings settings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
+        StreamExecutionEnvironment stremEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        //TableEnvironment tableEnv = TableEnvironment.create(settings);
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(stremEnv, settings);
+
+
 
         String name            = "myhive";
         String defaultDatabase = "default";
@@ -23,5 +31,45 @@ public class FlinkAndHiveIntegration {
 
         System.out.println(tableEnv.getCurrentCatalog());
         System.out.println(tableEnv.getCurrentDatabase());
+
+
+       /* *
+         * emp
+         * emp1
+         * emp2
+         * emp3
+         * emp4
+         * emp5
+         * mykafka*/
+
+        String[] strings = tableEnv.listTables();
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        Table table = tableEnv.sqlQuery("select * from emp1");
+
+
+
+        table.printSchema();
+
+
+        /**
+         * 18> 9999,shufang
+         * 19> 8888,lanyage
+         * 1> 8888,yangtao
+         * 20> 8888,yangtao
+         * 7> 1001,su
+         * 1> 1002,batman
+         * 24> 1001,superman
+         * 3> 8888,lanyage
+         * 2> 9999,shufang
+         */
+        tableEnv.toAppendStream(table, Row.class).print();
+
+        stremEnv.execute("");
+
+
+
     }
 }
