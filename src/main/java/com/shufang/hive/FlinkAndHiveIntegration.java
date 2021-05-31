0f6b6f3b1a0951ee5e8c6319dfc94f898a990a6e
shufangreal <shufangreal@126.com>
2021-05-31 22:42:23 +0800

    this is my 9 commit

--- src/main/java/com/shufang/hive/FlinkAndHiveIntegration.java
+++ src/main/java/com/shufang/hive/FlinkAndHiveIntegration.java
@@ -1,5 +1,6 @@
 package com.shufang.hive;
 
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.Table;
@@ -10,17 +11,18 @@ import org.apache.flink.types.Row;
 
 public class FlinkAndHiveIntegration {
     public static void main(String[] args) throws Exception {
-        EnvironmentSettings settings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
+        System.setProperty("HADOOP_USER_NAME", "shufang");
+
+        EnvironmentSettings settings = EnvironmentSettings.newInstance().useBlinkPlanner().inBatchMode().build();
         StreamExecutionEnvironment stremEnv = StreamExecutionEnvironment.getExecutionEnvironment();
-        //TableEnvironment tableEnv = TableEnvironment.create(settings);
-        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(stremEnv, settings);
+        TableEnvironment tableEnv = TableEnvironment.create(settings);
+//        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(stremEnv, settings);
 
 
-
-        String name            = "myhive";
+        String name = "myhive";
         String defaultDatabase = "default";
-        String hiveConfDir     = "src/main/resources"; // a local path
-        String version         = "1.2.2";
+        String hiveConfDir = "src/main/resources"; // a local path
+        String version = "1.2.2";
 
         HiveCatalog hive = new HiveCatalog(name, defaultDatabase, hiveConfDir, version);
         tableEnv.registerCatalog("myhive", hive);
@@ -33,42 +35,38 @@ public class FlinkAndHiveIntegration {
         System.out.println(tableEnv.getCurrentDatabase());
 
 
-       /* *
+        /* *
          * emp
          * emp1
          * emp2
          * emp3
          * emp4
          * emp5
-         * mykafka*/
+         * mykafka
+         * */
 
-        String[] strings = tableEnv.listTables();
+/*        String[] strings = tableEnv.listTables();
         for (String string : strings) {
             System.out.println(string);
         }
 
-        Table table = tableEnv.sqlQuery("select * from emp1");
+        Table mykafka = tableEnv.sqlQuery("select * from mykafka");
+        mykafka.printSchema();*/
 
 
 
-        table.printSchema();
+
+        Table mytable = tableEnv.from("mytable");
+        mytable.printSchema();
 
 
-        /**
-         * 18> 9999,shufang
-         * 19> 8888,lanyage
-         * 1> 8888,yangtao
-         * 20> 8888,yangtao
-         * 7> 1001,su
-         * 1> 1002,batman
-         * 24> 1001,superman
-         * 3> 8888,lanyage
-         * 2> 9999,shufang
-         */
-        tableEnv.toAppendStream(table, Row.class).print();
 
-        stremEnv.execute("");
+//        tableEnv.sqlUpdate("insert into mytable values ('shufang',36.2)");
 
+        mytable.insertInto("mytable");
+        tableEnv.execute("");
+
+        //stremEnv.execute("");
 
 
     }
